f4d3e24c0c0b5f2b1be3e72859219c7ec157e1d7
kenji <gunsecondstage0106@gmail.com>
2016-11-26 14:26:46 +0900

    complete sample app.

--- actions/index.js
+++ actions/index.js
@@ -7,18 +7,18 @@ export const addTodo = (text) => {
     id: nextTodoId++,
     text
   };
-};
+}
 
 export const toggleTodo = (id) => {
   return {
     type: 'TOGGLE_TODO',
     id
   };
-};
+}
 
 export const setVisibilityFilter = (filter) => {
   return {
     type: 'SET_VISIBILITY_FILTER',
     filter
   };
-};
+}
--- index.js
+++ index.js
@@ -1,32 +1,15 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import { addTodo, toggleTodo, setVisibilityFilter } from './actions/index.js';
+import todoApp from './reducers';
+import App from './components/App';
 
-let store = createStore(function() { return 'Hello' });
+let store = createStore(todoApp);
 
-var addTodoElem = document.getElementById('addTodo');
-var input = addTodoElem.getElementByTagName('input')[0];
-var button = addTodoElem.getElementByTagName('button')[0];
-
-button.addEventListener('click', () => {
-  var todoText = input.value;
-  store.dispatch(addTodo(todoText));
-});
-
-var todoList = document.getElementById('todoList');
-var elements = todoList.getElementByTagName('li');
-var listArray = [...elements];
-listArray.forEach((v, index) => {
-  v.addEventListener('click', e => {
-    store.dispatch(toggleTodo(index));
-  });
-}) ;
-
-var links = document.getElementById('links');
-var childs = links.childNodes;
-var childList = [...childs];
-childList.filter(v => v.nodeName != '#text').forEach(v => {
-  v.addEventListener('click', e => {
-    var filterText = v.innerHTML;
-    store.dispatch(setVisibilityFilter(filterText));
-  });
-});
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
